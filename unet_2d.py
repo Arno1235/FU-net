f1ae79db76c6c63456ea5dd391cf619fac6ee926
Arno1235 <arno.ve2000@gmail.com>
2023-03-22 10:31:16 +0100

    test loss function

--- unet_2d.py
+++ unet_2d.py
@@ -314,8 +314,8 @@ class Model:
             flat_probs = tf.reshape(probs, [-1, self.n_class])
 
             # TEST
-            print(f'flat_probs: {flat_probs}')
-            print(f'flat_labels: {flat_labels}')
+            print(f'flat_probs: {np.shape(flat_probs)} {flat_probs}')
+            print(f'flat_labels: {np.shape(flat_labels)} {flat_labels}')
 
             if self.weight_type == 'feedback':
                 weight_map = feedback_weight_map(flat_probs, flat_labels, 3, 100)
